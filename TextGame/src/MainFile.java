5a224b3b7a5cd6c101d00672fd0aab45531314af
rrao24 <rao.ram5718@gmail.com>
2015-12-23 16:20:26 -0500

    All classes

--- TextGame/src/MainFile.java
+++ TextGame/src/MainFile.java
@@ -1,7 +1,18 @@
 
 public class MainFile {
 	
-	private String s;
-	private String str;
+	public static void main(String[] args)
+	{
+		System.out.print("how many rooms?");
+	}
 
 }
+
+//room 1 person who tells you which direction to go (don't know if he's telling the truth or not)
+//room 2 start room exit room
+//room 3 slow room, 2 turns to get out multiple types of slow rooms
+//room 4 blank room
+//room 5 random sending room
+//room 6 fairy room (you know is good)
+
+//timed mode, race mode vs cpu or person, battle mode (stay away)
